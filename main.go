9a3f099ba96fdc9bb45eeccbf558fddd40aa8635
Swati Pal <swatipal39@gmail.com>
2024-09-06 16:29:39 +0530

    Added variables, constant and formatted the output using Printf

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main //Give a package name to which this file beongs to
 import "fmt"
 
 func main() {
-
-	fmt.Println("Welcome to our conference booking application") //fmt is the package that contains Println method(Uppercase P in Println method 
-																//indicates that it's a public method)
+	//Introducing variable
+	var conferenceName string = "Go Conference"
+	const tickets = 50
+	remainingTickets := 40
+	fmt.Println("Welcome to our",conferenceName,"booking application") //fmt is the package that contains Println method(Uppercase P in Println method indicates that it's a public method)
+	fmt.Printf("We have %v tickets in total and left with %v tickets now!\n",tickets, remainingTickets)
+	fmt.Println("Get your ticket to attend.")
+																
 	
 }
