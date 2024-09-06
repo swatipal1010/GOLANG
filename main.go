d6be49c90a5eb8580ff98e5ba9909d76109eb288
Swati Pal <swatipal39@gmail.com>
2024-09-06 16:35:48 +0530

    Added variables with their datatype declaration as the values are assigned later on

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ func main() {
 	const tickets = 50
 	remainingTickets := 40
 	fmt.Println("Welcome to our",conferenceName,"booking application") //fmt is the package that contains Println method(Uppercase P in Println method indicates that it's a public method)
-	fmt.Printf("We have %v tickets in total and left with %v tickets now!\n",tickets, remainingTickets)
+	fmt.Printf("We have %v tickets in total and %v tickets are still available!\n",tickets, remainingTickets)
 	fmt.Println("Get your ticket to attend.")
+
+
+	var username string			//When we don't assign a value to a variable immediately, we need to mention its datatype
+	var userTicket uint			//uint allows for assigning postive integers only
+	//ask for username
+	username = "John"
+	userTicket = 10
+	fmt.Println(username)
+	fmt.Println(userTicket)
 																
 	
 }
