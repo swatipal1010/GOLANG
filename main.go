460a6d86c27a745078ace7f1ec4a172fd429aecf
Swati Pal <swatipal39@gmail.com>
2024-09-07 11:46:33 +0530

    Added sendTicket func to send the copy of booked ticket to user. Also used sleep function to mimic the background processing of generating pdf of ticket and then sending it over email

--- main.go
+++ main.go
@@ -2,18 +2,21 @@ package main //Give a package name to which this file beongs to
 
 import (
 	"fmt"
+	"time"
 )
 
 // Introducing package-level variables(remove these variables from func. call and func. parameter list as they are accessible from here globally)
 var conferenceName string = "Go Conference"
+
 const conferenceTickets int = 50
+
 var remainingTickets uint = 50
 
 // var bookings [50]string								//Equivalent var bookings = [50]string{}
-var bookings = make([]UserData, 0)			//This is a empty slice of UserData struct. It doesn't have a fixed size rather it grows as per neeed.
+var bookings = make([]UserData, 0) //This is a empty slice of UserData struct. It doesn't have a fixed size rather it grows as per neeed.
 														//Equ. to writing 'bookings := []map[string]string{}'
 
-type UserData struct{
+type UserData struct {
 	firstName string
 	lastName string
 	email string
@@ -30,7 +33,7 @@ func main() {
 		fmt.Println("Get your ticket to attend.")
 	*/
 
-	for { //Can mention condition after for loop 'for remainingTickets>0 && len(bookings)<=50'
+	for { 													//Can mention condition after for loop 'for remainingTickets>0 && len(bookings)<=50'
 		//Get user input
 		firstName, lastName, email, userTicket := getUserInput()
 
@@ -40,6 +43,8 @@ func main() {
 		if isValidName && isValidEmail && isValidTicketNumber {
 			//Book user ticket
 			bookTicket(firstName, lastName, email, userTicket)
+			//Send email ticket to user
+			sendTicket(firstName, lastName, email, userTicket)
 
 			//Use the function 'printFirtNames' to print firts names of all bookings
 			firstNames := getFirstNames()
@@ -75,15 +80,15 @@ func greetUsers() {
 
 // Function to return firstname(slice of string values) of all the bookings
 func getFirstNames() []string {
-	firstNames := []string{}           //slice to store only firstname of the customers
+	firstNames := []string{} //slice to store only firstname of the customers
 
 	for _, booking := range bookings { //We get 'index'(_) and 'value'(booking) while iterating through bookings slice using 'range' iterator
 
 		//var names = strings.Fields(booking) //'names' is an array that contains 2 values(firstname & lastname) divided on whitespace seperator
 		//using Fields method
 
-		firstNames = append(firstNames, booking.firstName)		//taking firstname from UserData struct
-		fmt.Printf("List of bookings %v\n",bookings)
+		firstNames = append(firstNames, booking.firstName) //taking firstname from UserData struct
+		fmt.Printf("List of bookings %v\n", bookings)
 	}
 	//fmt.Printf("The first name of all bookings are: %v\n",firstNames)
 	return firstNames
@@ -124,10 +129,10 @@ func bookTicket(firstName string, lastName string, email string, userTicket uint
 
 	//Create map to store user data
 	var userData = UserData{
-		firstName : firstName,
-		lastName : lastName,
-		email : email,
-		numberOfTickets : userTicket,
+		firstName: firstName,
+		lastName: lastName,
+		email: email,
+		numberOfTickets: userTicket,
 	}
 
 	//Adding elements in slice rather than in array
@@ -142,4 +147,13 @@ func bookTicket(firstName string, lastName string, email string, userTicket uint
 	fmt.Printf("Thank You %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTicket, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
+}
+
+// Function to send ticket to the users who booked the conference tickets
+func sendTicket(firstName string, lastName string, email string, userTicket uint) {
+	time.Sleep(5 * time.Second)												//mimicing sleep of some time for background processing to take place
+	var ticket = fmt.Sprintf("%v tickets for %v %v", userTicket, firstName, lastName) //Sprintf helps to save the string following it in a var
+	fmt.Println("##############################################")
+	fmt.Printf("Sending ticket:\n %v \nto email address %v\n", ticket, email)
+	fmt.Println("##############################################")
 }
